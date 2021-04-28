3d95397d9bed649f725bf6eaec239c675f6f2fa9
Jiacheng Huang <huangjiacheng0709@outlook.com>
2021-04-28 16:45:39 +0800

    Pacman finally appears on display

--- software/pacman/main.c
+++ software/pacman/main.c
@@ -13,35 +13,28 @@
 #include "usb_kb/usb_ch9.h"
 #include "usb_kb/USB.h"
 
-void control_menu()
+void read_rgbas_from_file(char *file_name, int *rgbas)
 {
-	printf("Control Menu\n");
-	printf("\t1. Fill in Illini orange (E84A27FF);\n");
-	printf("\t2. Fill in Illini blue (13294BFF).\n");
-	for (;;) {
-		printf("Please enter one of the above options: ");
-		int option = 0;
-		scanf("%d", &option);
-		if (option == 1) {
-			int control = 0x10000E42;
-			for (int y = 0; y < 16; ++y) {
-				for (int x = 0; x < 16; ++x) {
-					IOWR_ALTERA_AVALON_PIO_DATA(CONTROL_BASE, control | (y << 12) | (x << 16));
-					printf("%x\n", control | (y << 12) | (x << 16));
-				}
-			}
-		} else if (option == 2) {
-			int control = 0x10000124;
-			for (int y = 0; y < 16; ++y) {
-				for (int x = 0; x < 16; ++x) {
-					IOWR_ALTERA_AVALON_PIO_DATA(CONTROL_BASE, control | (y << 12) | (x << 16));
-					printf("%x\n", control | (y << 12) | (x << 16));
-				}
-			}
-		} else {
-			printf("Command not found.\n");
-		}
+	FILE *file = fopen(file_name, "r");
+	for (int i = 0; i < 256; ++i) {
+		fscanf(file, "%x", rgbas + i);
+		printf("%x\n", rgbas[i]);
 	}
+	fclose(file);
+}
+
+void read_rgbas_from_stdin(int *rgbas)
+{
+	for (int i = 0; i < 256; ++i) {
+		scanf("%x", rgbas + i);
+		printf("%x\n", rgbas[i]);
+	}
+}
+
+int rgba_to_rgb(int rgba)
+{
+	int rgb = ((rgba & 0xF0000000) >> 20) | ((rgba & 0x00F00000) >> 16) | ((rgba & 0x0000F000) >> 12);
+	return rgb;
 }
 
 int main()
@@ -49,42 +42,17 @@ int main()
 	printf("PacPac\n");
 	int illini_blue = 0x13294BFF;
 	int illini_orange = 0xE84A27FF;
+	int pacman[256] = {0};
+	printf("Please enter pacman:\n");
+	read_rgbas_from_stdin(pacman);
 	int control = 0;
-	control = 0x10000124;
+	control = 0x10100000;
 	for (int y = 0; y < 16; ++y) {
 		for (int x = 0; x < 16; ++x) {
-			IOWR_ALTERA_AVALON_PIO_DATA(CONTROL_BASE, control | (y << 12) | (x << 16));
-			printf("%x\n", control | (y << 12) | (x << 16));
+			int actual_control = control | (y << 12) | (x << 16) | rgba_to_rgb(pacman[y * 16 + x]);
+			IOWR_ALTERA_AVALON_PIO_DATA(CONTROL_BASE, actual_control);
+			printf("%x\n", actual_control);
 		}
 	}
-	control = 0x10100E42;
-	for (int y = 0; y < 16; ++y) {
-		for (int x = 0; x < 16; ++x) {
-			IOWR_ALTERA_AVALON_PIO_DATA(CONTROL_BASE, control | (y << 12) | (x << 16));
-			printf("%x\n", control | (y << 12) | (x << 16));
-		}
-	}
-	control = 0x20000001;
-	for (int y = 0; y < 30; ++y) {
-		for (int x = 0; x < 40; ++x) {
-			IOWR_ALTERA_AVALON_PIO_DATA(CONTROL_BASE, control | (y << 16) | (x << 22));
-			printf("%x\n", control | (y << 16) | (x << 22));
-		}
-	}
-	// 0x29dd0000
-	// 0010 1001 1101 1101 0000 0000 0000 0000
-	control = 0x20000000;
-	IOWR_ALTERA_AVALON_PIO_DATA(CONTROL_BASE, control | (16 << 16) | (16 << 22));
-	// 0010 0000 0100 0001 0000 0000 0000 0001
-	printf("%x\n", control | (2 << 16) | (2 << 22));
-	// IOWR_ALTERA_AVALON_PIO_DATA(CONTROL_BASE, control | (1 << 16) | (1 << 22));
-	/*
-	for (int y = 1; y < 2; ++y) {
-		for (int x = 1; x < 2; ++x) {
-			IOWR_ALTERA_AVALON_PIO_DATA(CONTROL_BASE, control | (y << 16) | (x << 22));
-			printf("%x\n", control | (y << 12) | (x << 16));
-		}
-	}
-	*/
 	return 0;
 }
