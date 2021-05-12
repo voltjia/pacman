2ef83fa5c59aeb93bf5d1eb933b5e47b0a0b2224
tanwenhao99 <tanwenhao99@gmail.com>
2021-05-11 21:23:52 -0500

    Delete ShiShan

--- software/pacman/main.c
+++ software/pacman/main.c
@@ -90,82 +90,6 @@ BYTE GetDriverandReport()
 	return device;
 }
 
-void setLED(int LED)
-{
-	IOWR_ALTERA_AVALON_PIO_DATA(LEDS_PIO_BASE,
-			(IORD_ALTERA_AVALON_PIO_DATA(LEDS_PIO_BASE) | (0x001 << LED)));
-}
-
-void clearLED(int LED)
-{
-	IOWR_ALTERA_AVALON_PIO_DATA(LEDS_PIO_BASE,
-			(IORD_ALTERA_AVALON_PIO_DATA(LEDS_PIO_BASE) & ~(0x001 << LED)));
-
-}
-
-void printSignedHex0(signed char value)
-{
-	BYTE tens = 0;
-	BYTE ones = 0;
-	WORD pio_val = IORD_ALTERA_AVALON_PIO_DATA(HEX_DIGITS_PIO_BASE);
-	if (value < 0) {
-		setLED(11);
-		value = -value;
-	} else {
-		clearLED(11);
-	}
-	//handled hundreds
-	if (value / 100)
-		setLED(13);
-	else
-		clearLED(13);
-
-	value = value % 100;
-	tens = value / 10;
-	ones = value % 10;
-
-	pio_val &= 0x00FF;
-	pio_val |= (tens << 12);
-	pio_val |= (ones << 8);
-
-	IOWR_ALTERA_AVALON_PIO_DATA(HEX_DIGITS_PIO_BASE, pio_val);
-}
-
-void printSignedHex1(signed char value)
-{
-	BYTE tens = 0;
-	BYTE ones = 0;
-	DWORD pio_val = IORD_ALTERA_AVALON_PIO_DATA(HEX_DIGITS_PIO_BASE);
-	if (value < 0) {
-		setLED(10);
-		value = -value;
-	} else {
-		clearLED(10);
-	}
-	//handled hundreds
-	if (value / 100)
-		setLED(12);
-	else
-		clearLED(12);
-
-	value = value % 100;
-	tens = value / 10;
-	ones = value % 10;
-	tens = value / 10;
-	ones = value % 10;
-
-	pio_val &= 0xFF00;
-	pio_val |= (tens << 4);
-	pio_val |= (ones << 0);
-
-	IOWR_ALTERA_AVALON_PIO_DATA(HEX_DIGITS_PIO_BASE, pio_val);
-}
-
-void setKeycode(WORD keycode)
-{
-	IOWR_ALTERA_AVALON_PIO_DATA(KEYCODE_BASE, keycode);
-}
-
 void randomly_change_direction(int *map, int x, int y) {
 	int sprite = map_get_sprite(map, x, y);
 	map_set_sprite(map, x, y, get_sprite((sprite & ~SPRITE_DIRECTION_MASK) | ((rand() % 4 + 1) << SPRITE_DIRECTION_SHIFT)));
@@ -580,7 +504,6 @@ int main()
 	srand (time(NULL));
 
 	BYTE rcode;
-	BOOT_MOUSE_REPORT buf;		//USB mouse report
 	BOOT_KBD_REPORT kbdbuf;
 
 	BYTE runningdebugflag = 0;//flag to dump out a bunch of information when we first get to USB_STATE_RUNNING
@@ -613,7 +536,6 @@ int main()
 		if (GetUsbTaskState() == USB_STATE_RUNNING) {
 			if (!runningdebugflag) {
 				runningdebugflag = 1;
-				setLED(9);
 				device = GetDriverandReport();
 			} else if (device == 1) {
 				//run keyboard debug polling
@@ -637,47 +559,11 @@ int main()
 					key0 = kbdbuf.keycode[1];
 					key1 = temp;
 				}
-//				setKeycode(kbdbuf.keycode[0]);
-//				printSignedHex0(kbdbuf.keycode[0]);
-//				printSignedHex1(kbdbuf.keycode[1]);
 				printf("\n");
-			}
-
-			else if (device == 2) {
-				rcode = mousePoll(&buf);
-				if (rcode == hrNAK) {
-					//NAK means no new data
-					continue;
-				} else if (rcode) {
-					printf("Rcode: ");
-					printf("%x \n", rcode);
-					continue;
-				}
-				printf("X displacement: ");
-				printf("%d ", (signed char) buf.Xdispl);
-				printSignedHex0((signed char) buf.Xdispl);
-				printf("Y displacement: ");
-				printf("%d ", (signed char) buf.Ydispl);
-				printSignedHex1((signed char) buf.Ydispl);
-				printf("Buttons: ");
-				printf("%x\n", buf.button);
-				if (buf.button & 0x04)
-					setLED(2);
-				else
-					clearLED(2);
-				if (buf.button & 0x02)
-					setLED(1);
-				else
-					clearLED(1);
-				if (buf.button & 0x01)
-					setLED(0);
-				else
-					clearLED(0);
 			}
 		} else if (GetUsbTaskState() == USB_STATE_ERROR) {
 			if (!errorflag) {
 				errorflag = 1;
-				clearLED(9);
 				printf("USB Error State\n");
 				//print out string descriptor here
 			}
@@ -690,7 +576,6 @@ int main()
 				USB_init();
 			}
 			errorflag = 0;
-			clearLED(9);
 		}
 	}
 
